c01ad17f9cc84b3b2d43e81e441a3dcd5a06ed17
Fadel <fadel.g.heji@gmail.com>
2022-04-10 23:58:56 -0700

    Mostly Done

--- index.js
+++ index.js
@@ -1,38 +1,83 @@
-function forEach() {
-
+function forEach(array, callBack) {
+ for (let i = 0; i<array.length; i++){
+      callBack(array[i],i, array)
+ }
 }
 
-function map() {
+function map(array, callBack) {
+  const newArray = []
 
+   for (let i=0; i<array.length; i++){
+        newArray.push(callBack(array[i]))
+   }
+   return newArray
 }
 
-function filter() {
+ function filter(array, callBack) {
+  const newArray = []
 
+  for (let item of array){
+    if(callBack(item)){
+      newArray.push(item)
+    }
+  }
+  return newArray
+ }
+
+ function every(array, callBack) {
+  
+  for (let i =0; i<array.length;i++){
+    let bool =null
+    bool =callBack(array[i])
+  if(!bool){
+    return false
+  } 
+  }return true
+}  
+
+function some(array, callBack) {
+  
+  for (let i =0; i<array.length;i++){
+    
+  if(callBack(array[i])){
+    return true
+  } 
+  }return false
+}  
+  
+
+function majority(array, callBack) {
+ let counter = 0
+  for (let i=0;i<array.length;i++){
+if (callBack(array[i])){
+  counter += 1
 }
-
-function every() {
-
+  }
+if (counter>array.length-counter){
+  return true
 }
-
-function some() {
-
+ return false
 }
+ 
 
-function majority() {
 
-}
+function once(callBack) {
+  return callBack()
+  }
 
-function once() {
+  function groupBy(array, callBack) {
+    const Obj = {}
+      for(let i=0;i<array.length;i++){
+      Obj[callBack(array[i])] = [array[i]]
+    } return Obj
+    }
 
-}
-
-function groupBy() {
-
-}
-
-function arrayToObject() {
-
-}
+function arrayToObject(array, callBack) {
+  let Obj = {}
+    for (i=0;i<array.length;i++){
+    Obj[array[i]]=callBack(array[i])
+  }return Obj
+  }
 
 module.exports = { 
   forEach, 
